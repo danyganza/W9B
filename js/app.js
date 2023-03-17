f4c0f7ffdd1a6c3df27d6aa94d35517c3f3de100
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-03-16 20:33:36 -0600

    added values

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Step 1: Declared 3 variables
+let name = "Danny";
+let age = 26;
+let job = "Basketball Coach";
+
+
+// Step 3: I gave valiables values 
+console.log(`My name is ${name}, I am ${age} years old, and I work as a ${job}.`);
